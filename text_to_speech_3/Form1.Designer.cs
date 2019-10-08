825e25fee6e0ba077f66b2fffefd579e231d0c06
Gilbert Melendez <titom305@yahoo.com>
2019-10-08 16:03:03 -0400

    Add project files.

---
+++ text_to_speech_3/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace text_to_speech_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btn_Speak = new System.Windows.Forms.Button();
+            this.btn_Resume = new System.Windows.Forms.Button();
+            this.btn_Pause = new System.Windows.Forms.Button();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(90, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(608, 256);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btn_Speak
+            // 
+            this.btn_Speak.Location = new System.Drawing.Point(45, 426);
+            this.btn_Speak.Name = "btn_Speak";
+            this.btn_Speak.Size = new System.Drawing.Size(175, 77);
+            this.btn_Speak.TabIndex = 1;
+            this.btn_Speak.Text = "Speak";
+            this.btn_Speak.UseVisualStyleBackColor = true;
+            this.btn_Speak.Click += new System.EventHandler(this.Btn_Speak_Click);
+            // 
+            // btn_Resume
+            // 
+            this.btn_Resume.Location = new System.Drawing.Point(252, 426);
+            this.btn_Resume.Name = "btn_Resume";
+            this.btn_Resume.Size = new System.Drawing.Size(175, 77);
+            this.btn_Resume.TabIndex = 2;
+            this.btn_Resume.Text = "Resume";
+            this.btn_Resume.UseVisualStyleBackColor = true;
+            this.btn_Resume.Click += new System.EventHandler(this.Btn_Resume_Click);
+            // 
+            // btn_Pause
+            // 
+            this.btn_Pause.Location = new System.Drawing.Point(455, 426);
+            this.btn_Pause.Name = "btn_Pause";
+            this.btn_Pause.Size = new System.Drawing.Size(175, 77);
+            this.btn_Pause.TabIndex = 3;
+            this.btn_Pause.Text = "Pause";
+            this.btn_Pause.UseVisualStyleBackColor = true;
+            this.btn_Pause.Click += new System.EventHandler(this.Btn_Pause_Click);
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.Location = new System.Drawing.Point(660, 426);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(175, 77);
+            this.btn_Stop.TabIndex = 4;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = true;
+            this.btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 564);
+            this.Controls.Add(this.btn_Stop);
+            this.Controls.Add(this.btn_Pause);
+            this.Controls.Add(this.btn_Resume);
+            this.Controls.Add(this.btn_Speak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btn_Speak;
+        private System.Windows.Forms.Button btn_Resume;
+        private System.Windows.Forms.Button btn_Pause;
+        private System.Windows.Forms.Button btn_Stop;
+    }
+}
+
---
+++ text_to_speech_3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace text_to_speech_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SpeechSynthesizer speechSynthesizerObj;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+            btn_Resume.Enabled = false;
+            btn_Pause.Enabled = false;
+            btn_Stop.Enabled = false;
+        }
+
+        private void Btn_Speak_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+            if(richTextBox1.Text!="")
+            {
+                speechSynthesizerObj = new SpeechSynthesizer();
+                //Asynchronously speaks the content present in RichTextBox1
+                speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                btn_Pause.Enabled = true;
+                btn_Stop.Enabled = true;
+            }
+        }
+
+        private void Btn_Pause_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                //Gets the current speaking state of the SpeechSyntesizer object.
+                if (speechSynthesizerObj.State == SynthesizerState.Speaking)
+                {
+                    //Pauses the SpeechSynthesizer object.
+                    speechSynthesizerObj.Pause();
+                    btn_Resume.Enabled = true;
+                    btn_Speak.Enabled = false;
+                }
+            }
+        }
+
+        private void Btn_Resume_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                if (speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    //Resumes the SpeechSynthesizer
+                    speechSynthesizerObj.Resume();
+                    btn_Resume.Enabled = false;
+                    btn_Speak.Enabled = true;
+                }
+            }
+        }
+
+        private void Btn_Stop_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj != null)
+            {
+                //Disposes the SpeechSynthesis object
+                speechSynthesizerObj.Dispose();
+                btn_Speak.Enabled = true;
+                btn_Resume.Enabled = false;
+                btn_Pause.Enabled = false;
+                btn_Stop.Enabled = false;
+            }
+        }
+    }
+}
